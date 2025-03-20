5d00d6a04a5896c3c86589659f5f6ecb644703f6
Anas Paracha <anasparacha85@gmail.com>
2025-03-20 14:38:17 -0700

    Update UserSignup.jsx

--- internee frontend/src/pages/Users/UserSignup.jsx
+++ internee frontend/src/pages/Users/UserSignup.jsx
@@ -130,7 +130,7 @@ return response.json()
             <h1 className='text-gray-800 text-center'>OR</h1>
             <button
   type="button"
-  onClick={() => window.location.href = "http://localhost:5000/api/auth/google"}
+  onClick={() => window.location.href = `${url}/api/auth/google`}
   className="w-full flex cursor-pointer
    items-center justify-center gap-2 bg-green-700 text-white py-3 rounded-md hover:bg-green-600 transition mt-3"
 >
