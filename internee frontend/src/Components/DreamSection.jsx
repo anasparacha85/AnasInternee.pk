952d0789eac6562a5fca2414e8f67c860cfd00d4
Anas Paracha <anasparacha85@gmail.com>
2025-03-21 06:06:54 -0700

    Update DreamSection.jsx

--- internee frontend/src/Components/DreamSection.jsx
+++ internee frontend/src/Components/DreamSection.jsx
@@ -35,9 +35,9 @@ const DreamSection = () => {
   
           {/* Buttons */}
           <div className="mt-6 flex gap-4 justify-center md:justify-start">
-          <button className="px-6 py-2 border border-green-600 text-gray-700 rounded-[30px] shadow-md hover:bg-gray-200">
+            <NavLink to="/job-portal">  <button className="px-6 py-2 border border-green-600 text-gray-700 rounded-[30px] shadow-md hover:bg-gray-200">
               Job Portal
-            </button>
+            </button></NavLink>
         <NavLink to='/LMS'> <button className="px-6 py-2 bg-green-600 text-white rounded-[30px] shadow-md hover:bg-green-700">
               Our LMS
             </button></NavLink>   
