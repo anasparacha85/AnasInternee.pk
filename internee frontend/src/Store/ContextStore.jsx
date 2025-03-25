838f05af92183a1f81f5eafe526f2f498ee80c8c
Anas Paracha <anasparacha85@gmail.com>
2025-03-25 12:50:33 -0700

    Update ContextStore.jsx

--- internee frontend/src/Store/ContextStore.jsx
+++ internee frontend/src/Store/ContextStore.jsx
@@ -3,7 +3,7 @@ import { useContext, createContext, useEffect, useState, useCallback } from "rea
 export const StoreContext = createContext();
 
 export const StoreContextProvider = ({ children }) => {
-  const url = "https://backend-2g0wab8jg-anasparacha85s-projects.vercel.app";
+  const url = "https://backendproj-five.vercel.app";
 
   const [AdminLoginOpen, setAdminLoginOpen] = useState(false);
   const [AdminSignupOpen, setAdminSignupOpen] = useState(false);
